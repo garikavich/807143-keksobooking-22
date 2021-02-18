946aae6d5475936677873070c7e48ee08bcf3091
Garkusha Oleg <38359645+garikavich@users.noreply.github.com>
2021-02-18 10:51:03 +0200

    fix more details

--- js/main.js
+++ js/main.js
@@ -3,14 +3,8 @@ const AVATAR = [
   'img/avatars/user01.png',
   'img/avatars/user02.png',
   'img/avatars/user03.png',
-  'img/avatars/use04.png',
-  'img/avatars/user05.png',
-  'img/avatars/user06.png',
-  'img/avatars/user07.png',
-  'img/avatars/user08.png',
 ]
 const TITLE = ['Чернигов', 'Киев', 'Одесса', 'Днепропетровск', 'Львов']
-const ADDRESS = ['{{location.x}}', '{{location.y}}']
 const PRICE = [100, 200]
 const TYPE = ['palace', 'flat', 'house', 'bungalow']
 const ROOMS = [1, 2, 3]
@@ -24,21 +18,12 @@ const FEATURES = [
   'washer',
   'elevator',
   'conditioner',
-  'bar24h',
-  'sport place',
-  'free wi-fi',
-  'wheelchair accessible',
-  'babysitting',
-  'conference center',
 ]
 const DESCRIPTION = ['Не далеко от центра', 'Далеко от центра', 'Рынок близко', 'ТЦ близко']
 const PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-  'http://o0.github.io/assets/images/kiev/hostel5.jpg',
-  'http://o0.github.io/assets/images/kiev/hostel7.jpg',
-  'http://o0.github.io/assets/images/kiev/hostel50.jpg',
 ]
 const LOCATION_X = [35.65000, 35.70000]
 const LOCATION_Y = [139.70000, 139.80000]
@@ -62,15 +47,11 @@ const generatesNumberInteger = function (min, max) {
 generatesNumberInteger(2, 8)
 
 const getSliceArray = function (array) {
-  return array.slice(0, generatesNumberInteger(0, array.length - 1)).join(', ')
+  return array.slice(0, generatesNumberInteger(0, array.length - 1))
 }
 
 const getRandomElementArray = function (array) {
   return array[generatesNumberInteger(0, array.length - 1)]
-}
-
-const getDataString = function (data) {
-  return data.join(', ')
 }
 
 const generateRandomData = function () {
@@ -80,7 +61,7 @@ const generateRandomData = function () {
     },
     offer: {
       title: getRandomElementArray(TITLE),
-      address: getDataString(ADDRESS),
+      address: 'location.x location.y',
       price: getRandomElementArray(PRICE),
       type: getRandomElementArray(TYPE),
       rooms: getRandomElementArray(ROOMS),
@@ -92,8 +73,8 @@ const generateRandomData = function () {
       photos: getSliceArray(PHOTOS),
     },
     location: {
-      locationX: generatesNumberFloat(LOCATION_X[0], LOCATION_X[1], 4),
-      locationY: generatesNumberFloat(LOCATION_Y[0], LOCATION_Y[1], 4),
+      x: generatesNumberFloat(LOCATION_X[0], LOCATION_X[1], 4),
+      y: generatesNumberFloat(LOCATION_Y[0], LOCATION_Y[1], 4),
     },
   }
 }
